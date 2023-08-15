dd6c477dce85bdbab48a0c1538947291a98b2c6a
granillo-eduardo <142277847+granillo-eduardo@users.noreply.github.com>
2023-08-15 18:10:37 -0300

    Se sube código HelloWorld
    Se sube código junto con los archivos de proyecto y solución de HelloWorld.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" \n \n                Hello World \n\n");
+            
+        }
+    }
+}
