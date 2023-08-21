c6ae848be11cca7e1a520e6a6b859a1916952fea
granillo-eduardo <142277847+granillo-eduardo@users.noreply.github.com>
2023-08-21 14:07:41 -0300

    Agregando ejercicios a la rama

---
+++ labNetPractica2/Ejercicio 2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dividendo;
+            string divisor;
+
+            Console.Write(" Ingrese un valor para el dividendo : ");
+            dividendo = Console.ReadLine();
+
+            Console.Write(" Ingrese un valor para el divisor : ");
+            divisor = Console.ReadLine();
+
+            Dividir(dividendo, divisor);
+
+            Console.ReadKey();
+        }
+        public static void Dividir(string dividendo, string divisor)
+        {
+            try
+            {
+                Double resultado;
+
+                resultado = Convert.ToDouble(dividendo) / Convert.ToDouble(divisor);
+                Console.WriteLine($"\n {dividendo} / {divisor} = {resultado}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("\n “¡Solo Chuck Norris divide por cero!” ");
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine($"Secapturo {ex.Message}");
+                //Console.WriteLine(Environment.NewLine);
+                Console.WriteLine($"Stack {ex.StackTrace}");
+            }
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine($"\n “¡Seguro Ingreso una letra o no ingreso nada!”. \n {ex.Message}");
+            //}
+
+            //catch (Exception ex)
+            //{
+            //    //excepcion propia
+            //    Console.WriteLine("\n " + ex.Message);               
+            //}
+            finally
+            {
+                Console.WriteLine("\n Finalizo la operación");
+            }
+
+        }
+
+
+
+    }
+}
---
+++ labNetPractica2/Excepciones.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        public static void Dividir(string dividendo, string divisor)
+        {
+            try
+            {
+                Double resultado;
+
+                resultado = Convert.ToDouble(dividendo) / Convert.ToDouble(divisor);
+                Console.WriteLine($"\n {dividendo} / {divisor} = {resultado}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("\n “¡Solo Chuck Norris divide por cero!” ");
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine($"Secapturo {ex.Message}");
+                //Console.WriteLine(Environment.NewLine);
+                Console.WriteLine($"Stack {ex.StackTrace}");
+            }
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine($"\n “¡Seguro Ingreso una letra o no ingreso nada!”. \n {ex.Message}");
+            //}
+
+            //catch (Exception ex)
+            //{
+            //    //excepcion propia
+            //    Console.WriteLine("\n " + ex.Message);               
+            //}
+            finally
+            {
+                Console.WriteLine("\n Finalizo la operación");
+            }
+
+        }
+    }
+}
